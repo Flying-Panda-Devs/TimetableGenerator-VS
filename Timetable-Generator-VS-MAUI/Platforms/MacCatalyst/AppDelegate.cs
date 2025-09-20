471cced56d373e56e792ec24b3eebfbc0ffc13d9
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-09-20 16:43:59 +0100

    Replace backend with MAUI

---
+++ Timetable-Generator-VS-MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Timetable_Generator_VS_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
